fcf7e3ae9f3ee52d1d2d41e776bb6d65fb071b81
Jeff Griffiths <jeff@eko-recordings.ca>
2026-05-01 20:40:59 +0000

    maybe working action file

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,3 @@
-
-import { join } from 'node:path'
-
-// All supported Bun targets:
-// const bun_targets: string[] = [
-// 	'bun-linux-x64',
-// 	'bun-linux-arm64',
-// 	'bun-windows-x64',
-// 	'bun-windows-arm64',
-// 	'bun-darwin-x64',
-// 	'bun-darwin-arm64',
-// 	'bun-linux-x64-musl',
-// 	'bun-linux-arm64-musl',
-// ]
-
 const config = {
     entrypoints: ['./index.ts'],
     outdir: `./dist/win-lib.exe`,
